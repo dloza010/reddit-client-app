adce0cf69674821729944967c4ea3133e71028bb
dloza010 <david.loza010@gmail.com>
2022-11-21 18:16:56 -0500

    created reddit slice with name, initial state, and reducers

--- src/Components/Posts/posts.js
+++ src/Components/Posts/posts.js
@@ -1,3 +1,9 @@
+import React, { useState } from "react";
 
 
+const Posts = (props) => {
 
+    const [voteValue, setVoteValue ] = useState(0);
+    
+
+}
---
+++ src/Store/redditSlice.js
@@ -0,0 +1,57 @@
+import {createSlice, createSelector} from '@reduxjs/toolkit';
+
+
+const initialState = {
+    posts: [],
+    error: false, 
+    isLoading: false,
+    searchTerm: '',
+    selectedSubreddit: '/r/pics/'
+};
+
+const redditSlice = createSlice({
+    name: 'redditPosts',
+    initialState,
+    reducers: {
+        setPosts(state, action) {
+            state.posts = action.payload;
+        },
+        startGetPosts(state) {
+            state.isLoading = true;
+            state.error = false;
+        },
+        getPostsSuccess(state, action) {
+            state.isLoading = false;
+            state.posts = action.payload;
+        },
+        getPostsFailed(state) {
+            state.isLoading = false;
+            state.error = true;
+        },
+        setSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+        setSelectedSubreddit(state, action) {
+            state.selectedSubreddit = action.payload;
+            state.searchTerm = '';
+        },
+        toggleShowingComments(state, action) {
+            state.posts[action.payload].showingComments = !state.posts[action.payload].showingComments;
+        },
+        startGetComments(state, action) {
+            state.posts[action.payload].showingComments = !state.posts[action.payload].showingComments;
+            if (!state.posts[action.payload].showingComments) { return; }
+            state.posts[action.payload].loadingComments = true;
+            state.posts[action.payload].error = false;
+        },
+        getCommentsSuccess(state, action) {
+            state.posts[action.payload.index].loadingComments = false;
+            state.posts[action.payload.index].comments = action.payload;
+        },
+        getCommentsFailed(state, action) {
+            state.posts[action.payload].loadingComments = false;
+            state.posts[action.payload].error = true;
+        }
+    },
+});
+
